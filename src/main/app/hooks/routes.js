bd1657f79cd656a7bb64b665c48b781b64d8fc00
dioguima <diogoferreiraguima@hotmail.com>
2018-10-03 06:18:40 -0300

    Issue message case added to webhook handling

--- src/main/app/hooks/routes.js
+++ src/main/app/hooks/routes.js
@@ -9,24 +9,35 @@ const router = Router({mergeParams: true})
 router.post('/', async (req, res) => {
   try {
     //console.log(req.body)
+    const repository = await findByGithubId(req.body.repository.id)
+    const threads = await getThreadsFromRepository(repository.id)
+    const mainThread = threads.filter((t) => t.name === "Main")[0]
+        
     if(req.body.commits != null){
-        const repository = await findByGithubId(req.body.repository.id)
-        const threads = await getThreadsFromRepository(repository.id)
-        const mainThread = threads.filter((t) => t.name === "Main")[0]
         //console.log(threads)
         //console.log(mainThread)
 
-        const commits = req.body.commits;
+        const commits = req.body.commits
         //console.log(commits)
         for(let i in commits){
             const commit = commits[i]
-            const userName = commit.author.username;
+            const userName = commit.author.username
             const content = `Um commit foi adicionado no repositório por ${userName}`
             const resp = await createMessage(content, mainThread._id, null, new Date())
             if(!resp){
                 res.status(200).send(resp)
             }
         }
+    } else if(req.body.issue != null && req.body.action === "opened"){
+        
+        const issue = req.body.issue
+        const userName = issue.user.login;
+        const content = `Um novo issue foi aberto por ${userName}`
+        const resp = await createMessage(content, mainThread._id, null, new Date())
+        if(!resp){
+            res.status(200).send(resp)
+        }
+        
     }
     res.status(200).send(true)
   } catch (err) {
